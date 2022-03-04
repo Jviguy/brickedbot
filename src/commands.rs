3fc7e65810bcffe6abfcf266b747974365069d5a
Jviguy <jviguytwo2@gmail.com>
2022-03-03 23:14:56 -0500

    start work

---
+++ src/commands.rs
@@ -0,0 +1,10 @@
+use serenity::model::interactions::application_command::{ApplicationCommandInteractionDataOptionValue, ApplicationCommandInteraction};
+use serenity::model::interactions::InteractionResponseType;
+use serenity::{
+    prelude::*
+};
+use serenity::utils::Colour;
+
+pub async fn link(ctx: &Context, command: &ApplicationCommandInteraction) -> Option<String> {
+    None
+}
---
+++ src/main.rs
@@ -0,0 +1,93 @@
+mod commands;
+
+use std::env;
+
+use serenity::{
+    async_trait,
+    model::{
+        gateway::Ready,
+        id::GuildId,
+        interactions::{
+            application_command::{
+                ApplicationCommandOptionType,
+            },
+            Interaction,
+            InteractionResponseType,
+        },
+    },
+    prelude::*
+};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+
+    async fn ready(&self, ctx: Context, _: Ready) {
+        let guild = GuildId(948931516031959062);
+        guild.set_application_commands(&ctx.http, |commands| {
+            commands
+                .create_application_command(|command| {
+                    command.name("ping").description("A ping command")
+                })
+                .create_application_command(|command| {
+                    command.name("query").description("Returns information on a given server.")
+                })
+        }).await.expect("Failed to make slash commands!");
+    }
+
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            let content = match command.data.name.as_str() {
+                "ping" => Some("pong!".to_string()),
+
+
+
+                _ => Some("cock".to_string())
+            };
+
+
+            match content {
+                None => {}
+                Some(content) => {
+                    if let Err(why) = command
+                        .create_interaction_response(&ctx.http, |response| {
+                            response
+                                .kind(InteractionResponseType::ChannelMessageWithSource)
+                                .interaction_response_data(|message| message.content(content))
+                        })
+                        .await
+                    {
+                        println!("Cannot respond to slash command: {}", why);
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // Configure the client with your Discord bot token in the environment.
+    let token = env::var("TOKEN").expect("Expected a token in the environment");;
+
+    // The Application Id is usually the Bot User Id.
+    let application_id: u64 = 949139214363131924;
+
+    // Build our client.
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .application_id(application_id)
+        .await
+        .expect("Error creating client");
+    {
+
+    }
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform
+    // exponential backoff until it reconnects.
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
