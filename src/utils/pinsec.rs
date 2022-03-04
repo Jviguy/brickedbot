ec46767f1ce04417bdaca144967a2aad314b6815
Jviguy <jviguytwo2@gmail.com>
2022-03-04 12:21:48 -0500

    adding tracing for the pinsec algo

--- src/utils/pinsec.rs
+++ src/utils/pinsec.rs
@@ -12,20 +12,24 @@ pub fn score(pin: i32) -> f32 {
         .take_while(|num| *num > 0)
         .map(|num| num % 10)
         .collect::<Vec<i32>>());
-    let mut previous: i32 = 10;
+    let mut previous: i32 = 100;
     for (idx,digit) in digits.iter().enumerate() {
         let digit = *digit;
+        println!("S: {}", s);
         // A 4 digit pin grows exponentially easier to guess if theres many repeated digits / in close distance
         if digit-previous.abs() < 3 {
             s *= 2.0;
+            println!("MULTPLYING BY 2");
         }
         // Lower digits are guessed first in most 10 digit code prompts or if theres a repeat of this digit somewhere in the pin.
         if digit < 5 || digits.binary_search(&digit).unwrap() != idx {
             s += 0.2;
+            println!("+ 0.2")
         }
         // The digit 0 adds complexity to codes based on its position in code prompts
         if digit == 0 {
             s -= 0.5;
+            println!("-0.5")
         }
         previous = digit;
     }
